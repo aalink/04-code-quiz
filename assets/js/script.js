44dc6acd9e8ad4e9bf8840ef569f379ee96ff002
Aaron Yacher <29718914+aalink@users.noreply.github.com>
2022-03-23 13:54:53 -0400

    Refactoring "option#" to "wrongOption#"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,34 @@
 const quizQuestion1 = {
     question: "Which principle of software development is aimed at reducing repetition of software patterns?",
     correct: "DRY",
-    option1: "AHA",
-    option2: "WET",
-    option3: "SRP",
+    wrongOption1: "AHA",
+    wrongOption2: "WET",
+    wrongOption3: "SRP",
 };
 
 const quizQuestion2 = {
     question: "Which programming language refers to key/value pairs as a dictionary?",
     correct: "Python",
-    option1: "JavaScript",
-    option2: "C++",
-    option3: "Java",
+    wrongOption1: "JavaScript",
+    wrongOption2: "C++",
+    wrongOption3: "Java",
 };
 
 const quizQuestion3 = {
     question: "Which company develops the Compute Unified Device Architecture, also known as CUDA?",
     correct: "Nvidia",
-    option1: "Microsoft",
-    option2: "Samsung",
-    option3: "Google",
+    wrongOption1: "Microsoft",
+    wrongOption2: "Samsung",
+    wrongOption3: "Google",
 
 };
 
 function askQuestion(questionNumber, attempt) {
     console.log(questionNumber.question)
-    console.log("A) " + questionNumber.option1)
-    console.log("B) " + questionNumber.option2)
-    console.log("C) " + questionNumber.option3)
-    console.log("D) " + questionNumber.option4)
+    console.log("A) " + questionNumber.wrongOption1)
+    console.log("B) " + questionNumber.wrongOption2)
+    console.log("C) " + questionNumber.wrongOption3)
+    console.log("D) " + questionNumber.wrongOption4)
     if (attempt === questionNumber.correct) {
         console.log("CORRECT")
     } else {
