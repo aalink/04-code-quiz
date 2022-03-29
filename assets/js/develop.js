001a3ae9e04a765e5d034921ae5881b70dc7a149
Aaron Yacher <29718914+aalink@users.noreply.github.com>
2022-03-29 12:57:58 -0400

    Adding vars, question array change, new functions. New function to register the choice the user clicks on. New function to end the quiz. Parameter change for displayQuestion function.

--- assets/js/develop.js
+++ assets/js/develop.js
@@ -1,4 +1,4 @@
-var count = 10;
+var count = 100;
 //  Select increment and decrement button elements
 var startButton = document.querySelector(".start-button");
 var timerCountDown = document.querySelector(".timer-count");
@@ -8,6 +8,8 @@ var currentQuestionIndex = 0;
 var score = 0;
 var scoreBox = document.querySelector(".timer-score-boxes");
 var h3ToDisplayQuestion = document.querySelector("h3");
+var ol = document.querySelector("#question-options");
+var endOfQuiz = document.querySelector(".quiz-box")
 
 function hideStartButton() {
   startButton.style.display = "none";
@@ -18,37 +20,65 @@ var question = [
     questionTitle:
       "Which principle of software development is aimed at reducing repetition of software patterns?",
     choices: ["DRY", "AHA", "WET", "SRP"],
-    answer: 0,
+    answer: "DRY",
   },
   {
     questionTitle:
       "Which programming language refers to key/value pairs as a dictionary?",
     choices: ["JavaScript", "C++", "Java", "Python"],
-    answer: 3,
+    answer: "Python",
   },
 
   {
     questionTitle:
       "Which company develops the Compute Unified Device Architecture, also known as CUDA?",
     choices: ["Microsoft", "Nvidia", "Samsung", "Google"],
-    answer: 1,
+    answer: "Nvidia",
   },
 ];
 
 function displayQuestion(index) {
+  ol.innerHTML = "";
+  h3ToDisplayQuestion.innerHTML = "";
   h3ToDisplayQuestion.append(question[index].questionTitle);
   console.log(question[index].questionTitle);
   for (let i = 0; i < 4; i++) {
-    ol = document.querySelector("#question-options");
     var li = document.createElement("li");
     var questionsToInterate = question[index].choices[i];
     li.textContent = questionsToInterate;
     ol.append(li);
-    li.attributes;
+    li.onclick = registerChoiceClick;
+    // li.attributes;
     console.log(question[0].choices[i]);
     // li.addEventListener
   }
 }
+
+function registerChoiceClick() {
+  console.log('working');
+  userChoice = this.textContent
+  console.log(userChoice);
+  if (userChoice === question[currentQuestionIndex].answer) {
+    console.log("CORRECT");
+  
+  } else {
+    console.log("WRONG");
+    count -= 10;
+  }
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex === question.length) {
+    // this will write a function to end the quiz.
+    endQuiz()
+  } else {
+    displayQuestion(currentQuestionIndex);
+  }
+}
+
+function endQuiz() {
+  endOfQuiz.setAttribute("class", "hide")
+}
+
 // displayQuestion(0);
 
 // FUNCTION
@@ -83,7 +113,7 @@ function displayQuestion(index) {
 // use set attribute when i need to clear the page and display a score.
 
 startButton.addEventListener("click", function () {
-  displayQuestion(0);
+  displayQuestion(currentQuestionIndex);
   timerCountDown.textContent = count;
   var timeInterval = setInterval(function () {
     count--;
